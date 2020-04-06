4b61d7b28eb17c692626ecbd479bdfe66d3d8026
MrBenz <dkostya05@gmail.com>
2020-04-06 13:17:37 +0300

    Update exchange_rate.py

--- `gg/exchange_rate.py
+++ `gg/exchange_rate.py
@@ -10,14 +10,16 @@ class CursParser:
         self.LINK = link
         self.content = req.get(self.LINK)
         self.soup = bf(self.content.text, 'html.parser')
-        self.valyta = valyta.lower
-
+        self.LINK = self.LINK.replace("eur", valyta)
 
     def get_bank(self):
-        self.LINK = self.LINK.replace("eur", self.valyta)
-        all_banks = soup.find_all("a", class_="font-bold")
-        all_sails = soup.find_all("div", class_="table-flex__cell table-flex__rate font-size-large   text-nowrap")
-        self.banks = ()
-        for bank in range(all_banks):
-            self.banks.append(str(bank["data-currencies-bank-name"]))
-            self.banks.append(str())
+        all_banks = self.soup.find_all("a", class_="font-bold")
+        all_sails = self.soup.find_all("div", class_="table-flex__cell table-flex__rate font-size-large   text-nowrap")
+        banks = []
+        for bank in all_banks:
+            banks.append(bank["data-currencies-bank-name"])
+        banks = tuple(banks)
+        return banks
+
+Cur = CursParser("eur")
+Cur.get_bank()
